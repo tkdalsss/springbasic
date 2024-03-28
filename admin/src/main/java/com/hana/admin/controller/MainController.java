d01439a7e2f1f1c5fe9ccbd8a871cfd073a5292a
tkdalsss <lsm991229@naver.com>
2024-03-28 10:23:01 +0900

    240328-1

---
+++ admin/src/main/java/com/hana/admin/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.hana.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    public String main() {
+        return "index";
+    }
+}
